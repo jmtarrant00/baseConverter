587a3239ee8312f888bdf4986ecd42db45bafca8
Jonathan Tarrant <jmtarrant00@gmail.com>
2022-04-15 13:59:12 -0400

    convert to smaller base

--- baseConverter.py
+++ baseConverter.py
@@ -3,19 +3,45 @@
 #   another. User specifies the base of
 #   the input and output
 
+from asyncio.windows_events import NULL
 import math
+from operator import concat
+
+charList = {}
+file = open("characterList.txt")
+key = 0
+
+for line in file:
+    charList[key] = line.rstrip()
+    key += 1
 
 inputBase = 10
 inputNumber = 0
 outputBase = 2
+outputNumber = None
 
 print('''This This program is allows you to convert numbers from one base to another.
-        the base must be between 2 and 50.''')
+        the base must be between 2 and 61.''')
 
 def inputs():
     inputBase = input("What is the base of the input? ")    
     inputNumber = input("What is the number to convert? ")
     outputBase = input("What is the base of the output? ")
 
-if (inputBase > 50 or inputBase < 2):
-    print("Please input a base between 2 and 50")
+def convert(inputBase, inputNumber, outputBase):
+    quotient = inputNumber
+    remainder = 0
+    if inputBase < outputBase:
+        while quotient != 0:
+            quotient = inputNumber // outputBase
+            remainder = inputNumber % outputBase
+            outputNumber = remainder + outputNumber
+
+
+while True:
+    inputs()
+    if (inputBase > 61 or inputBase < 2):
+        print("Please input a base between 2 and 50")
+        continue
+    else:
+        convert(inputBase, inputNumber, outputBase)
