db243be96bca1b0a8e70e19b72290eb8ed45f3d0
Starwolf16 <jmtarrant00@gmail.com>
2022-04-23 00:27:00 -0400

    THINGz

--- baseConverter.py
+++ baseConverter.py
@@ -6,7 +6,6 @@
 charList = {}
 file = open("characterList.txt")
 key = 0
-
 for line in file:
     charList[key] = line.rstrip()
     key += 1
@@ -19,6 +18,11 @@ outputNumber = ""
 print('''This program is allows you to convert numbers from one base to another.
 >>The base must be between 2 and 61.''')
 
+def get_key(val):
+    for key, value in charList.items():
+        if val == value:
+            return key
+
 def inputs():
     global inputBase, inputNumber, outputBase
     inputBase = int(input("What is the base of the input? "))
@@ -29,22 +33,27 @@ def inputs():
 def convert(inputBase, inputNumber, outputBase, outputNumber):
     #Take input base and number and output base and number as variables
     #  convert input number to decimal then convert that to the output base
-
-
-    quotient = inputNumber
     remainder = 0
+    quotient = inputNumber
+    # inputLength = len(inputNumber)
+    # if inputBase != 10:
+    #     for x in inputLength:
+    #         inputDec = (charList.index())
+    # else:
+    #     quotient = inputNumber
+
     while quotient != 0:
+        print("While Loop")
         remainder = quotient % outputBase
         quotient = quotient // outputBase
-        outputNumber = charList[remainder] + outputNumber
+        outputNumber = charList.get(remainder) + outputNumber
     # elif (inputBase < outputBase):
         
-
     print(str(inputNumber) + " in base " + str(outputBase) + " is " + outputNumber)
 
 
 inputs()
-print("inputNumber ", inputNumber)
+print("inputNumber", inputNumber)
 if (int(inputBase) > 61 or int(inputBase) < 2):
     print("Please input a base between 2 and 50")
     #continue
