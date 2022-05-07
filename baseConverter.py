aa2f0d5a96996f66b69ae988b91ae9b5336649aa
Jonathan Tarrant <jmtarrant00@gmail.com>
2022-05-06 23:31:57 -0400

    Full Base Conversion

--- baseConverter.py
+++ baseConverter.py
@@ -10,9 +10,9 @@ for line in file:
     charList[key] = line.rstrip()
     key += 1
 
-inputBase = 10
+inputBase = '10'
 inputNumber = 0
-outputBase = 2
+outputBase = '2'
 outputNumber = ""
 
 print('''This program is allows you to convert numbers from one base to another.
@@ -25,36 +25,38 @@ def get_key(val):
 
 def inputs():
     global inputBase, inputNumber, outputBase
-    inputBase = int(input("What is the base of the input? "))
-    inputNumber = input("What is the number to convert? ")
-    outputBase = int(input("What is the base of the output? "))
+    inputBase = int(input("What is the base of the input? ") or 10)
+    inputNumber = str(input("What is the number to convert? ") or 0)
+    outputBase = int(input("What is the base of the output? ") or 2)
 
+def convertFrom10(quotient, outputBase):
+    remainder = 0
+    outputNumber = ""
+    while quotient != 0:
+        # print("While Loop")
+        remainder = quotient % outputBase
+        quotient = quotient // outputBase
+        outputNumber = charList.get(remainder) + outputNumber
+    return outputNumber
+    
 
-def convert(inputBase, inputNumber, outputBase, outputNumber):
+def convert(inputBase, inputNumber, outputBase, outputNumber = '0'):
     #Take input base and number and output base and number as variables
     #  convert input number to decimal then convert that to the output base
-    remainder = 0
-    quotient = inputNumber
     inputLength = len(inputNumber)
     inputDec = 0
+    inputRev = inputNumber[::-1]
     if inputBase != 10:
         for x in range(inputLength-1, -1, -1):
-            print("x", x)
-            for y in inputNumber[::-1]:
-                print("y", y)
-                inputDec = inputDec + (get_key(y) + (pow(inputBase, x)))
+            # print("x", x)
+            inputDec = inputDec + (get_key(inputRev[x]) * (pow(inputBase, x)))
         print("InputDec:", inputDec)
-    else:
         if outputBase == 10:
-            outputNumber = inputDec
+            outputNumber = str(inputDec)
         else:
-            quotient = inputDec
-            while quotient != 0:
-                print("While Loop")
-                remainder = quotient % outputBase
-                quotient = quotient // outputBase
-                outputNumber = charList.get(remainder) + outputNumber
-    # elif (inputBase < outputBase):
+            outputNumber = convertFrom10(inputDec, outputBase)
+    else:
+        outputNumber = convertFrom10(int(inputNumber), outputBase)
         
     print(str(inputNumber) + " in base " + str(outputBase) + " is " + outputNumber)
 
@@ -62,7 +64,7 @@ def convert(inputBase, inputNumber, outputBase, outputNumber):
 inputs()
 print("inputNumber", inputNumber)
 if (int(inputBase) > 61 or int(inputBase) < 2):
-    print("Please input a base between 2 and 50")
+    print("Please input a base between 2 and 61")
     #continue
 else:
     convert(inputBase, inputNumber, outputBase, outputNumber)
